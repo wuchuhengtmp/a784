c5eeb46f24bc6051331c653f5a9f3d81a3cf4e7a
吴楚衡 <wuchuheng@163.com>
2019-10-15 11:53:49 +0800

    【bug】修复禁用账号还能登录的情况

--- app/Http/Middleware/CheckToken.php
+++ app/Http/Middleware/CheckToken.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use App\Models\Tokens;
+use App\Models\Members;
 use Illuminate\Http\Response;
 class CheckToken
 {
@@ -25,6 +26,20 @@ class CheckToken
                 'message' => '您的账号已经在别的设备上登录了',
                 'status_code' => 403,
             ]);
+        } 
+        $user = Members::find($hasToken->member_id);
+        if (!$user) {
+            return response()->json([
+                'message' => '用户不存在',
+                'status_code' => 403,
+            ]);
+        }
+        if ($user['status'] == 0) {
+            return response()->json([
+                'message' => '您的账号已被禁用，如有疑问请联系管理员',
+                'status_code' => 403,
+            ]);
+
         }
         return $next($request);
     }
